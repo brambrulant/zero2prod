ff9c7776ea0c2209fd82cc74edf5d703c0b0ac6a
bramschabbink <bram@trunkrs.nl>
2025-01-06 23:18:19 +0100

    fix clippy

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
 fn main() {
     println!("Hello, world!");
-    let c = add_two_strings(String::from("Hello"), String::from("world") );
-    println!("{}", c)
-}
-
-fn add_two_strings(a: String, b: String) -> String {
-    a + &", " + &b + &"!"
 }
